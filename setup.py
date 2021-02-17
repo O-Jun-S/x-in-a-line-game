6f09d78df40615b1e3d8b2d1fb89b807fe5344f2
O-Jun-S <junsannnn3562@icloud.com>
2021-02-17 18:36:54 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ox-game',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Ojun',
+    author_email='',
+    description='X in a row game.'
+)
